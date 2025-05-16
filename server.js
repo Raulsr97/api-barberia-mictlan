9470c0362d31d1e2d6771c4901a9d2e084a6d934
Raul Salinas <salinasraul883@gmail.com>
2025-05-16 05:35:57 -0600

    Configuración inicial del proyecto

---
+++ server.js
@@ -0,0 +1,24 @@
+require('dotenv').config()
+const express = require('express')
+const sequelize = require('./config/database')
+const app = express()
+
+// middleware
+app.use(express.json())
+
+// Test de la conexion a la base de datos
+sequelize
+  .authenticate()
+  .then(() => console.log('Conectado a la base de datos'))
+  .catch((err) => console.error('Error al conectar a la base de datos', err))
+
+// Ruta de prueba
+app.get('/', (req, res) => {
+  res.send('API  de la barberia esta funcionando')
+})
+
+// Iniciar el servidor
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+})
